c759fc57cf55db01cb9c9736e3cd5ba6849aa53c
Gomez, Bethy <bethygomez@usf.edu>
2019-08-28 22:56:31 -0400

    Version 4 - maybe now this worked

--- HelloWorld4/Program.cs
+++ HelloWorld4/Program.cs
@@ -6,7 +6,13 @@ namespace HelloWorld4
     {
         static void Main(string[] args)
         {
+            // Pause to display Hello World!
+            // Why doesn't the solution file appear when I publish to GitHub!!
+            // Ahhhhh!!!
+            Console.WriteLine("Press any key to continue...");
             Console.WriteLine("Hello World!");
+            Console.ReadKey(true);
+            // I followed all of the instructions!
         }
     }
 }
